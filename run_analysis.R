8e7790283158d1c1592c3d7f39fb33e9e31f1b86
loutfik <loutfi.kuret@zsassociates.com>
2016-10-16 23:25:07 -0400

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,67 @@
+#Merges the training and the test sets to create one data set
+
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+x_test <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
+
+features <- read.table('./data/UCI HAR Dataset/features.txt')
+
+activityLabels = read.table('./data/UCI HAR Dataset/activity_labels.txt')
+
+colnames(x_train) <- features[,2] 
+colnames(y_train) <-"activityId"
+colnames(subject_train) <- "subjectId"
+
+colnames(x_test) <- features[,2] 
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+
+colnames(activityLabels) <- c('activityId','activityType')
+
+mrg_train <- cbind(y_train, subject_train, x_train)
+mrg_test <- cbind(y_test, subject_test, x_test)
+setAllInOne <- rbind(mrg_train, mrg_test)
+
+#Extracts only the measurements on the mean and standard deviation for each measurement
+
+colNames <- colnames(setAllInOne)
+
+mean_and_std <- (grepl("activityId" , colNames) | 
+                   grepl("subjectId" , colNames) | 
+                   grepl("mean.." , colNames) | 
+                   grepl("std.." , colNames) 
+)
+
+setForMeanAndStd <- setAllInOne[ , mean_and_std == TRUE]
+
+#Appropriately labels the data set with descriptive variable names
+
+setWithActivityNames <- merge(setForMeanAndStd, activityLabels,
+                              by='activityId',
+                              all.x=TRUE)
+
+#From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+TidyData <- aggregate(. ~subjectId + activityId, setWithActivityNames, mean)
+TidyData <- TidyData[order(TidyData$subjectId, TidyData$activityId),]
+
+
+write.table(TidyData, "TidyData.txt", row.name=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
